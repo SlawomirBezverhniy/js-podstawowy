9b3f7c06a0079b419a27426b5a2e251b3243466e
Slawek Bezverhniy <slavik.bezverhniy25@gmail.com>
2022-11-06 09:13:18 +0100

    first commit

---
+++ 01-html-css-podstawy/js/main.js
@@ -0,0 +1,41 @@
+//alert('Helo Slawek');
+
+
+console.log('Helo Slawek')
+
+
+// 1. okna dialogowe
+
+//alert('Helo')
+
+// const myName = prompt ('Jak masz na imie?');
+
+//console.log(myName);
+
+    //tworzenie zdan zmiennych
+    //1 sposob - za pomoca znaka + 
+
+//console.log('Hello ' + myName);
+
+    //2 za pomoca tzw templeta string (ES6)
+//console.log(`Helo ${myName}`);
+
+    //Warunki
+
+    //jednym z typow zmiennych jest boolean (prawda/falsz)
+
+    //instrukcje warunkowe
+
+    const number1 = prompt('podaj nomer');
+    const number2  = prompt('podaj nomer jeszcze raz')
+
+    console.log(number1);
+    console.log(number2);
+
+
+    
+   
+    
+    
+    
+
